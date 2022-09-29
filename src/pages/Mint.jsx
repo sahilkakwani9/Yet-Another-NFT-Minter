c649ae8d5a88d4ac20d4f16e33e2b7ec0c940401
sahilkakwani9 <sahkakwani@gmail.com>
2022-09-29 19:07:24 +0530

    Wrote Contract

--- src/pages/Mint.jsx
+++ src/pages/Mint.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
+import { useState } from 'preact/hooks';
 
 export default function Mint() {
+  const [NFT, setNFT] = useState(null);
+  const [NFTurl, setNFTurl] = useState(null);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [attributes, setAttributes] = useState('');
+
+  const handleMediaChange = async (e) => {
+    e.preventDefault();
+    if (e.target.files[0]) {
+      setNFT(e.target.files[0]);
+      setNFTurl(URL.createObjectURL(e.target.files[0]));
+      console.log(NFTurl);
+    }
+  }
+
   return (
     <div className='h-screen w-screen text-center'>
         <h1 className='font-Inter pt-28 font-extrabold text-7xl mb-16'>Mint NFT</h1>
         <div className='flex flex-col gap-5 justify-center items-center'>
             <div className='flex gap-5'>
-              <input type="text" placeholder="Name" class="px-3 py-3 placeholder-slate-500 text-slate-600 relative bg-white bg-white rounded text-md border-2 border-black shadow outline-none focus:outline-none focus:ring focus:border-white w-96"/>
-              <input type="text" placeholder="Description" class="px-3 py-3 placeholder-slate-500 text-slate-600 relative bg-white bg-white rounded text-md border-2 border-black shadow outline-none focus:outline-none focus:ring focus:border-white w-96"/>
+              <input type="text" placeholder="Name"   onChange={(e) => setName(e.target.value)} class="px-3 py-3 placeholder-slate-500 text-slate-600 relative bg-white rounded text-md border-2 border-black shadow outline-none focus:outline-none focus:ring focus:border-white w-96"/>
+              <input type="text" placeholder="Description" onChange={(e) => setDescription(e.target.value)} class="px-3 py-3 placeholder-slate-500 text-slate-600 relative bg-white bg-white rounded text-md border-2 border-black shadow outline-none focus:outline-none focus:ring focus:border-white w-96"/>
             </div>
             <div className='flex gap-5 w-1/3'>
-              <input type="text" placeholder="Attributes" class="px-3 py-3 placeholder-slate-500 text-slate-600 relative bg-white bg-white rounded text-md border-2 border-black shadow outline-none focus:outline-none focus:ring focus:border-white w-full"/>
+              <input type="text" placeholder="Attributes" onChange={(e) => setAttributes(e.target.value)} class="px-3 py-3 placeholder-slate-500 text-slate-600 relative bg-white bg-white rounded text-md border-2 border-black shadow outline-none focus:outline-none focus:ring focus:border-white w-full"/>
             </div>
             <div>
               <h5 className='text-start ml-2 text-lg font-bold'>Media</h5>
-              <input type="file" placeholder="Name" class="px-3 py-3 placeholder-slate-500 text-slate-600 relative bg-white bg-white rounded text-md border-2 border-black shadow outline-none focus:outline-none focus:ring focus:border-white w-96"/>
+              <input type="file" onChange={handleMediaChange} class="px-3 py-3 placeholder-slate-500 text-slate-600 relative bg-white rounded text-md border-2 border-black shadow outline-none focus:outline-none focus:ring focus:border-white w-96"/>
             </div>
         </div>
         <button type="button" class="mt-16 tracking-wider font-Inter border-0 font-extrabold text-2xl bg-gradient-to-r from-green-400 to-blue-500">Mint NFT</button>
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -5,6 +5,7 @@ import Mint from './Mint';
 
 export default function Wallet() {
   const [walletConnected, setWalletConnected] = useState(false)
+  const [currentAccount, setCurrentAccount] = useState(null);
 
 
 
@@ -35,7 +36,7 @@ export default function Wallet() {
                         signer = provider.getSigner();
                         setCurrentAccount(accounts[0]);
                         setWalletConnected(true);
-                        state.setCurrentAccount(accounts[0])
+                        // state.setCurrentAccount(accounts[0])
                         console.log(currentAccount);
                     }
                 })
@@ -43,7 +44,7 @@ export default function Wallet() {
             if (chaindId === 80001) {
                 setWalletConnected(true);
                 setCurrentAccount(getAccount[0]);
-                state.setCurrentAccount(getAccount[0]);
+                // state.setCurrentAccount(getAccount[0]);
             }
             console.log(walletConnected)
         }
@@ -57,7 +58,7 @@ export default function Wallet() {
       <div className='text-center pt-36 justify-center items-center'>
         <h1 className='font-Inter font-extrabold text-7xl mb-12'>Yet Another NFT Minter💀</h1>
         <div className='h-36 w-36 text-center justify-center m-auto'>
-          <img src="../../public/blueguy.svg" alt="" />
+          <img src="./blueguy.svg" alt="" />
         </div>
         <button className='mt-24 py-4 text-2xl bg-blue-500 hover:bg-blue-400 text-white font-bold px-6 border-b-8 border-blue-700 hover:border-blue-500 rounded' onClick={connectWallet}>Connect Wallet</button>
       </div>
